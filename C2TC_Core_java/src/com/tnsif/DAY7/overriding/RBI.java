66cd0ff2e6ad584a905d0031cb641d62ae664c29
Harikrushnan2005 <harikrishnan.t.25@gmail.com>
2025-10-03 17:38:40 +0530

    commit

---
+++ C2TC_Core_java/src/com/tnsif/DAY7/overriding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package com.tnsif.DAY7.overriding;
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
